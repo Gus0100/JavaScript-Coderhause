d9ab700a02dd94053864ba2a678a43d5f1c6f41a
Gustavo Flores <gustavoflores750@gmail.com>
2023-03-08 21:45:23 -0300

    Segunda Pre-Entrega

--- Primera Pre-Entrega/main.js
+++ Primera Pre-Entrega/main.js
@@ -1,17 +1,18 @@
 alert("Bienvenidos a SAMSUNG!");
 let nombreCliente = prompt("Ingrese su nombre");
 
-const celulares = ["S21","S22", "S23"]
+const celulares = ["S21", "S22", "S23"]
 
 const celularesInfo = [
-    {Nombre: "S21", Precio: 21000, Cantidad: 3},
-    {Nombre: "S22", Precio: 22000, Cantidad: 3},
-    {Nombre: "S23", Precio: 23000, Cantidad: 3},
+    {ID: 1 ,Nombre: "S21", Precio: 21000, Cantidad: 3 },
+    {ID: 2, Nombre: "S22", Precio: 22000, Cantidad: 3 },
+    {ID: 3, Nombre: "S23", Precio: 23000, Cantidad: 3 },
 ];
 
 
-class Samsung{
-    constructor(nombre, precio, cantidad, info) {
+class Samsung {
+    constructor(id, nombre, precio, cantidad, info) {
+        this.id = id;
         this.nombre = nombre;
         this.precio = precio;
         this.cantidad = cantidad;
@@ -19,45 +20,64 @@ class Samsung{
     }
 }
 
-const celular21 = new Samsung("S21", 21000, 3);
-const celular22 = new Samsung("S22", 22000, 3);
-const celular23 = new Samsung("S23", 23000, 3);
+const celular21 = new Samsung("1", "S21", 21000, 3);
+const celular22 = new Samsung("2", "S22", 22000, 3);
+const celular23 = new Samsung("3", "S23", 23000, 3);
 
 let auriculares = 500;
 
-let modeloElegido = prompt(`Hola ${nombreCliente} estos son los productos disponibles:
-${celulares}
-escriba el nombre del modelo que quiere adquirir`);
+let modeloElegido = prompt(`Hola ${nombreCliente}, estos son los productos disponibles:
+1. S21
+2. S22
+3. S23
+Ingrese el número correspondiente al modelo que desea adquirir:`);
 
-function ModeloElegido(){
-    while(modeloElegido != celulares){
+function ModeloElegido() {
+    while (modeloElegido != celularesInfo) {
         alert("Modelo incorrecto.");
-        modeloElegido = prompt(`Hola ${nombreCliente} estos son los productos disponibles:
-        ${celulares}
-        escriba el nombre del modelo que quiere adquirir`);
-        if(modeloElegido == celulares){
-            break
-        }
-        return modeloElegido
+        modeloElegido = prompt(`Hola ${nombreCliente}, estos son los productos disponibles:
+        1. S21
+        2. S22
+        3. S23
+        Ingrese el número correspondiente al modelo que desea adquirir:`);
     }
 }
 
 ModeloElegido()
 
-function InfoModeloElegido(){
-    
+function InfoModeloElegido() {
+    switch (modeloElegido) {
+        case "1":
+            alert(celular21.info);
+            break;
+
+        case "2":
+            alert(celular22.info);
+            break;
+
+        case "3":
+            alert(celular23.info);
+            break;
+
+        default:
+            ModeloElegido();
+    }
+    return modeloElegido;
 }
 
 InfoModeloElegido()
 
 
+let opcionCarrito = prompt("¿Quiere agregarlo al carrito?")
+
 let promocion = prompt(`${nombreCliente} no te pierdas esta oportunidad única llevando un ${modelo} se te aplica un descuento a unos auriculares por $${auriculares} \n ¿Quieres llevarlo?`);
 
-function OpcionCarrito(){
-    if(preguntaCarrito == "si"){
+
+function OpcionCarrito() {
+    if (preguntaCarrito == "si") {
         alert(`${modelo} añadido al carrito con éxito`);
         promocion = prompt(`${nombreCliente} no te pierdas esta oportunidad única llevando un ${modelo} se te aplica un descuento a unos auriculares por $${auriculares} \n ¿Quieres llevarlo?`);
-    }else{
+    } else {
         alert("bueno chau");
     }
 }
@@ -66,24 +86,24 @@ function OpcionCarrito(){
 OpcionCarrito()
 
 
-let resultado = (S23 && S22 && S21 + auriculares )
+let resultado = (S23 && S22 && S21 + auriculares)
 
-function OpcionPromocion(){
-    if(promocion == "si"){
+function OpcionPromocion() {
+    if (promocion == "si") {
         alert(`Tu monto total es de ${resultado}`);
         alert(`Hasta luego ${nombreCliente}, que disfrute de la promocion`);
-    }else{
-        alert("Tu monto a abonar es de " + S23 ||  S22 || S21);
+    } else {
+        alert("Tu monto a abonar es de " + S23 || S22 || S21);
         alert(`Hasta luego ${nombreCliente}, que lo disfrute`);
     }
 }
 
 let otroModelo = prompt(`${nombreCliente} ¿Quieres elegir otro modelo?`);
 
-function OpcionOtroModelo(){
-    if(otroModelo == "no"){
+function OpcionOtroModelo() {
+    if (otroModelo == "no") {
         alert(`Hasta luego ${nombreCliente}`);
-    }else{
+    } else {
         modelo = prompt(`Hola devuelta ${nombreCliente} ingrese el nombreCliente de los 3 modelos disponibles: "S23", "S22", "S21"`);
     }
     return modelo;
